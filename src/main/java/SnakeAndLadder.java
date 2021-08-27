ba85957c69a20eb87525b48e8f9893696078fe9c
jumdemonali <jumdemonali@gmail.com>
2021-08-27 18:54:01 +0530

    Modified: Dice Roll

--- src/main/java/SnakeAndLadder.java
+++ src/main/java/SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder
     {
         System.out.println("Welcome to snake and ladder program");
         int startPosition = 0;
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Die Rolled:" + diceNumber);
     }
 }
