81d5df6d8913ac5d87a6bf0347f70726592071d5
jumdemonali <jumdemonali@gmail.com>
2021-08-27 19:06:49 +0530

    Added: Option check

--- src/main/java/SnakeAndLadder.java
+++ src/main/java/SnakeAndLadder.java
@@ -4,7 +4,17 @@ public class SnakeAndLadder
     {
         System.out.println("Welcome to snake and ladder program");
         int startPosition = 0;
-        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-        System.out.println("Die Rolled:" + diceNumber);
+        int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Die Rolled:" + dieRoll);
+        int checkOption = (int) Math.floor(Math.random() * 10) % 3;
+        if (checkOption == 0) {
+            System.out.println("No Play");
+        } else if (checkOption == 1) {
+            System.out.println("Ladder Comes");
+            startPosition += dieRoll;
+        } else {
+            System.out.println("Snake Comes");
+            startPosition -= dieRoll;
+        }
     }
 }
