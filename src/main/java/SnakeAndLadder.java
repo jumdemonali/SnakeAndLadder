0b08c6deeb83ca18e8fdb703aa12ef653ce8d9b0
jumdemonali <jumdemonali@gmail.com>
2021-08-27 20:37:12 +0530

    Added: Two Players

--- src/main/java/SnakeAndLadder.java
+++ src/main/java/SnakeAndLadder.java
@@ -1,33 +1,42 @@
 public class SnakeAndLadder {
+    public static final int IS_SNAKE = 1;
+    public static final int IS_LADDER = 2;
+    public static final int WINNING_POSITION = 100;
+
     static int diceCount = 0;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to snake and ladder program");
-        int startPosition = 0;
-        while (startPosition != 100) {
-            diceCount++;
-            int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-            System.out.println("Die Rolled:" + dieRoll);
-            int checkOption = (int) Math.floor(Math.random() * 10) % 3;
-            if (checkOption == 0) {
-                System.out.println("No Play");
-            } else if (checkOption == 1) {
-                System.out.println("Ladder Comes");
-                if (startPosition + dieRoll <= 100) {
-                    startPosition += dieRoll;
-                } else if (startPosition + dieRoll > 100) {
-                    continue;
-                }
-            } else {
-                System.out.println("Snake Comes");
-                if ((startPosition - dieRoll) < 0) {
-                    startPosition = 0;
-                } else {
-                    startPosition -= dieRoll;
-                }
-            }
-            System.out.println(dieRoll);
-            System.out.println("Player Position After Dice Roll " + startPosition);
+        System.out.println("Welcome to snake and ladder project");
+
+        int player1Position = 0;
+// int player2Position = 0;
+        while (player1Position < WINNING_POSITION) {
+            ++diceCount;
+            player1Position = getPosition(player1Position);
         }
-        System.out.println("Number Of times Dice Roll To Win " + diceCount);
+
+    }
+
+    private static int getPosition(int position) {
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        int option = (int) (Math.floor(Math.random() * 10) % 3);
+        if (option == IS_SNAKE) {
+            System.out.println("Snake for the player");
+
+            if (position - diceNumber > 0) {
+                position -= diceNumber;
+            }
+        } else if (option == IS_LADDER) {
+            System.out.println("Ladder for the player");
+
+            if (position + diceNumber <= 100) {
+                position += diceNumber;
+            }
+        } else {
+            System.out.println("No play for the player");
+        }
+        System.out.println("dice count " + diceCount);
+        System.out.println("player1Position " + position);
+        return position;
     }
 }
